cf2647fadcce438b8d259f176fe8356ed358a4b6
Duy Nguyen <dnguyen0304@gmail.com>
2017-10-18 12:05:17 -0400

    Added fromAllNodes.

--- src/main/java/com/github/dnguyen0304/graph/find/path/all/FindAllPathStrategy.java
+++ src/main/java/com/github/dnguyen0304/graph/find/path/all/FindAllPathStrategy.java
@@ -74,4 +74,23 @@ public class FindAllPathStrategy {
         return paths;
     }
 
+    public List<List<Integer>> fromAllNodes(
+        List<List<Integer>> adjacencyMatrix) {
+
+        List<List<Integer>> paths = new ArrayList<>();
+
+        // Base Case #1: null or zero-valued adjacency matrices
+        if (adjacencyMatrix == null || adjacencyMatrix.isEmpty()) {
+            // Should this throw IllegalArgumentException instead?
+            return null;
+        }
+
+        // Iterative Case #1
+        for (int row = 0; row < adjacencyMatrix.size(); row++) {
+            paths.addAll(fromOneNode(adjacencyMatrix, row, row));
+        }
+
+        return paths;
+    }
+
 }
--- src/test/java/com/github/dnguyen0304/graph/find/path/all/FindAllPathStrategyTest.java
+++ src/test/java/com/github/dnguyen0304/graph/find/path/all/FindAllPathStrategyTest.java
@@ -78,13 +78,10 @@ public class FindAllPathStrategyTest {
 
     @Test
     public void testFromOneNodeAcross3Nodes() {
-        //   A B C D E F
-        // A 0 1 1 0 0 0
-        // B 0 0 1 1 1 0
-        // C 0 0 0 0 0 1
-        // D 0 0 0 0 0 0
-        // E 0 0 0 0 0 0
-        // F 0 0 0 0 0 0
+        //   A B C
+        // A 0 1 1
+        // B 0 0 1
+        // C 0 0 0
         List<Integer> aRow = new ArrayList<Integer>();
         aRow.add(0);
         aRow.add(1);
@@ -104,8 +101,9 @@ public class FindAllPathStrategyTest {
         this.expectedPaths.add(Arrays.asList(0, 1, 2));
         this.expectedPaths.add(Arrays.asList(0, 2));
 
-        List<List<Integer>> paths = this.strategy.fromOneNode(adjacencyMatrix,
-                                                              0);
+        List<List<Integer>> paths = this.strategy.fromOneNode(
+            this.adjacencyMatrix,
+            0);
         assertEquals(expectedPaths, paths);
     }
 
@@ -125,8 +123,125 @@ public class FindAllPathStrategyTest {
 
         this.expectedPaths.add(Arrays.asList(0, 1));
 
-        List<List<Integer>> paths = this.strategy.fromOneNode(adjacencyMatrix,
-                                                              0);
+        List<List<Integer>> paths = this.strategy.fromOneNode(
+            this.adjacencyMatrix,
+            0);
+        assertEquals(expectedPaths, paths);
+    }
+
+    @Test
+    public void testFromAllNodesReturnsNullOnNullAdjacencyMatrix() {
+        List<List<Integer>> paths = this.strategy.fromAllNodes(null);
+        assertNull(paths);
+    }
+
+    @Test
+    public void testFromAllNodesReturnsNullOnZeroValuedAdjacencyMatrix() {
+        List<List<Integer>> paths = this.strategy.fromAllNodes(
+            this.adjacencyMatrix);
+        assertNull(paths);
+    }
+
+    @Test
+    public void testFromAllNodesAcross3Nodes() {
+        //   A B C
+        // A 0 1 1
+        // B 0 0 1
+        // C 0 0 0
+        List<Integer> aRow = new ArrayList<Integer>();
+        aRow.add(0);
+        aRow.add(1);
+        aRow.add(1);
+        List<Integer> bRow = new ArrayList<Integer>();
+        bRow.add(0);
+        bRow.add(0);
+        bRow.add(1);
+        List<Integer> cRow = new ArrayList<Integer>();
+        cRow.add(0);
+        cRow.add(0);
+        cRow.add(0);
+        this.adjacencyMatrix.add(aRow);
+        this.adjacencyMatrix.add(bRow);
+        this.adjacencyMatrix.add(cRow);
+
+        this.expectedPaths.add(Arrays.asList(0, 1, 2));
+        this.expectedPaths.add(Arrays.asList(0, 2));
+        this.expectedPaths.add(Arrays.asList(1, 2));
+
+        List<List<Integer>> paths = this.strategy.fromAllNodes(
+            this.adjacencyMatrix);
+        assertEquals(expectedPaths, paths);
+    }
+
+    @Test
+    public void testFromAllNodesAcross6Nodes() {
+        //   A B C D E F
+        // A 0 1 1 0 0 0
+        // B 0 0 1 1 1 0
+        // C 0 0 0 0 0 1
+        // D 0 0 0 0 0 0
+        // E 0 0 0 0 0 0
+        // F 0 0 0 0 0 0
+        List<Integer> aRow = new ArrayList<Integer>();
+        aRow.add(0);
+        aRow.add(1);
+        aRow.add(1);
+        aRow.add(0);
+        aRow.add(0);
+        aRow.add(0);
+        List<Integer> bRow = new ArrayList<Integer>();
+        bRow.add(0);
+        bRow.add(0);
+        bRow.add(1);
+        bRow.add(1);
+        bRow.add(1);
+        bRow.add(0);
+        List<Integer> cRow = new ArrayList<Integer>();
+        cRow.add(0);
+        cRow.add(0);
+        cRow.add(0);
+        cRow.add(0);
+        cRow.add(0);
+        cRow.add(1);
+        List<Integer> dRow = new ArrayList<Integer>();
+        dRow.add(0);
+        dRow.add(0);
+        dRow.add(0);
+        dRow.add(0);
+        dRow.add(0);
+        dRow.add(0);
+        List<Integer> eRow = new ArrayList<Integer>();
+        eRow.add(0);
+        eRow.add(0);
+        eRow.add(0);
+        eRow.add(0);
+        eRow.add(0);
+        eRow.add(0);
+        List<Integer> fRow = new ArrayList<Integer>();
+        fRow.add(0);
+        fRow.add(0);
+        fRow.add(0);
+        fRow.add(0);
+        fRow.add(0);
+        fRow.add(0);
+        this.adjacencyMatrix.add(aRow);
+        this.adjacencyMatrix.add(bRow);
+        this.adjacencyMatrix.add(cRow);
+        this.adjacencyMatrix.add(dRow);
+        this.adjacencyMatrix.add(eRow);
+        this.adjacencyMatrix.add(fRow);
+
+        this.expectedPaths.add(Arrays.asList(0, 1, 2, 5));
+        this.expectedPaths.add(Arrays.asList(0, 1, 3));
+        this.expectedPaths.add(Arrays.asList(0, 1, 4));
+        this.expectedPaths.add(Arrays.asList(0, 2, 5));
+        this.expectedPaths.add(Arrays.asList(1, 2, 5));
+        this.expectedPaths.add(Arrays.asList(1, 3));
+        this.expectedPaths.add(Arrays.asList(1, 4));
+        this.expectedPaths.add(Arrays.asList(2, 5));
+
+        List<List<Integer>> paths = this.strategy.fromAllNodes(
+            this.adjacencyMatrix);
         assertEquals(expectedPaths, paths);
     }
 
